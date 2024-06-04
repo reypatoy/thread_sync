b440de5f8dc41438de0615324fa661d3f4848680
patoyrey <rey_patoy@bpoc.co.jp>
2024-06-04 15:56:51 +0800

    Separating application.properties for local and for prod

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #Simple crud application using java(springboot)
+
+#Running local command: mvn spring-boot:run -D"spring-boot.run.profiles"=local
+#Running production command: mvn spring-boot:run
--- src/main/java/com/threadsync/project/configuration/OpenAPIConfig.java
+++ src/main/java/com/threadsync/project/configuration/OpenAPIConfig.java
@@ -1,34 +1,26 @@
 package com.threadsync.project.configuration;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class OpenAPIConfig {
-  
-    @Value("${openapi.dev-url}")
-    private String devUrl;
 
-  @Value("${openapi.prod-url}")
-  private String prodUrl;
+  @Value("${openapi.url}")
+  public String url;
 
   @Bean
   public OpenAPI myOpenAPI() {
-    Server devServer = new Server();
-    devServer.setUrl(devUrl);
-    devServer.setDescription("Server URL in Development environment");
-   
-    Server prodServer = new Server();
-    prodServer.setUrl(prodUrl);
-    prodServer.setDescription("Server URL in production environment");
-
+      Server server = new Server();
+      server.setUrl(url);
+      server.setDescription("Server URL");
     return new OpenAPI()
-                .servers(List.of(devServer, prodServer));
+        .servers(List.of(server));
   }
 }
